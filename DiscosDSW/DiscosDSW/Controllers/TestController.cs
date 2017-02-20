6ddfb1ba8d8c8cac24e2bfd772b577062fc63f4c
Javigracia <neodreamer@gmail.com>
2017-02-20 14:01:40 +0100

    Version Inicial

---
+++ DiscosDSW/DiscosDSW/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using DiscosDSW.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DiscosDSW.Controllers
+{
+
+    public class TestController : Controller
+    {
+        public ActionResult GetView()
+        {
+            Cliente c = new Cliente();
+            c.Nombre = "Cliente 1";
+            c.Email = "email 1";
+            c.Password = "xxxxxx";
+            ViewData["Cliente"] = c;
+            return View("MyView");
+        }
+    }
+}
---
+++ DiscosDSW/DiscosDSW/Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DiscosDSW.Models
+{
+    public class Cliente
+    {
+        public override string ToString()
+        {
+            return this.Nombre + "|" + this.Email + "|" + this.Password;
+        }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
